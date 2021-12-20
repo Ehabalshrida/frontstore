f2f3b29455a4c38bfd028d1fb9e8129635a833f0
ehabalshrida <ehabshridah@gmail.com>
2021-12-20 18:31:41 +0200

    lab37

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import {combineReducers, createStore} from 'redux';
 import categoriesReducer from './categories';
 import productsReducer from './products';
+import cartReducer from './cart'
 
-//1-  combine reducers in one reducer 
-let reducers = combineReducers({categoriesReducer,productsReducer});
+let reducers = combineReducers({categoriesReducer,productsReducer,cartReducer});
 
-// 2- createStore 
 const store = () => {
     return createStore(reducers)
 }
 
-// 3 export 
 export default store();
